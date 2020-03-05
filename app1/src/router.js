5b13c38972999c6ad21731b9bd7cd4774725871c
Damon Du <604821528@qq.com>
2020-03-05 14:12:32 +0800

    相对路径会出问题。替换之。

--- app1/src/router.js
+++ app1/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 // Vue.use(Router)
 export default new Router({
@@ -11,6 +12,11 @@ export default new Router({
       path: '/app1',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     },
   ]
 })
---
+++ app1/src/views/About.vue
@@ -0,0 +1,25 @@
+
+<script>
+export default {
+  name: 'about',
+  data(){
+    return {
+      checkd1:true
+    }
+  },
+  render(h){
+    let self =this;
+    return h('el-checkbox', {
+      props:{
+        value:self.checked1
+      },
+      on: {
+        input: function (event) {
+          console.log(event)
+          self.checkd1=event
+        }
+      }
+    })
+  }
+}
+</script>
